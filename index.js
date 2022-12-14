dc1212460cac3228c9ecc3f5006f31bf4843b219
TheJobGG <jobglez80@gmail.com>
2022-12-14 14:40:14 -0700

    Add comments and delete req parameter from queryData function

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
 const express = require('express');
-const connection = require('./database');
 
+// Connection from database.js file
+const connection = require('./database');
 const app = express();
 
 app.get('/', (req, res) => res.send('<h1>Home</h1>'))
 
+// Get all brands
 app.get('/brand', (req, res) => {
+  
   const query = 'SELECT * FROM brand;';
-  queryData(req, res, query)
+  queryData(res, query)
 })
 
+// Get all products
 app.get('/products', (req, res) => {
   const query = 'SELECT * FROM product;';
-  queryData(req, res, query)
+  queryData(res, query)
 })
 
+// Get all users
 app.get('/users', (req, res) => {
   const query = 'SELECT * FROM user;';
-  queryData(req, res, query)
+  queryData(res, query)
 })
 
+// Get all products with his brand and who added it
 app.get('/relation', (req, res) => {
   const query =
     `select product.id, product.name, brand.name, user.username created_by
   from brand, user
   join product
   where product.brand_id = brand.brand_id and product.created_by = user.id;`;
-  queryData(req, res, query)
+  queryData(res, query)
 })
 
 
-
+// Start express server and initialize connection to database
 app.listen(3000, (req, res) => {
-  console.log('App listening on por 3000');
+  console.log('App listening on port 3000');
   connection.connect(function (err) {
     if (err) throw err;
     console.log('Connected to the MySQL server.');
@@ -40,11 +46,9 @@ app.listen(3000, (req, res) => {
 })
 
 
-
-
-function queryData(req, res, query) {
+function queryData(res, query) {
   connection.query(query, (error, result) => {
-    if (error) throw Error('An error has occurred during querying with database.')
+    if (error) res.send('An error has occurred during querying with database.').statusCode(500)
     
     res.send(result)
   })
