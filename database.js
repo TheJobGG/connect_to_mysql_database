8434e50ef61079114ac119df73dc441fde5e588d
TheJobGG <jobglez80@gmail.com>
2022-12-14 14:39:35 -0700

    Add comments and change replace characters

--- database.js
+++ database.js
@@ -1,20 +1,21 @@
 require('dotenv').config();
+const { createConnection } = require('mysql');
 
-const mysql = require('mysql');
-
+// Clean single quotes, double quotes, and commas from environment variables
 const envVariables = {
-  host:     process.env.MYSQL_HOST.replace(/'|,/g,''),
-  user:     process.env.MYSQL_USER.replace(/'|,/g,''),
-  password: process.env.MYSQL_PASSWORD.replace(/'|,/g,''),
-  database: process.env.MYSQL_DATABASE.replace(/'|,/g,''),
+  host: process.env.MYSQL_HOST.replace(/'|,|"/g, ''),
+  user: process.env.MYSQL_USER.replace(/'|,|"/g, ''),
+  password: process.env.MYSQL_PASSWORD.replace(/'|,|"/g, ''),
+  database: process.env.MYSQL_DATABASE.replace(/'|,|"/g, ''),
 }
 
-const connection = mysql.createConnection({
-    host: envVariables.host,
-    user: envVariables.user,
-    password: envVariables.password,
-    database: envVariables.database,
+// Create connection to database
+const connection = createConnection({
+  host: envVariables.host,
+  user: envVariables.user,
+  password: envVariables.password,
+  database: envVariables.database,
 });
 
-
+// Export connection
 module.exports = connection;
